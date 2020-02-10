6e20bc7a29c7b87620c5eb49a23c3c9ce4646fb6
Sean Keegan <sean@das.llc>
2020-02-10 13:36:30 +0200

    Added 'read one' functionality to task manager'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,10 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id].to_i)
+  end
+
   # TO DO:
   #  other basic CRUD actions
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Route for showing *all* tasks
-  get 'tasks', to: 'tasks#index'
+  get 'tasks',    to: 'tasks#index'
 
   # Route for showing single tasks
-  # get...
+  get 'task/:id', to: 'tasks#show'
 
   # routes for adding a new task
   # get...
