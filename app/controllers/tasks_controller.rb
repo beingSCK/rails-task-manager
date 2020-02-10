3da274cc32e000c4000c56a173ff22f1c8b4814d
Sean Keegan <sean@das.llc>
2020-02-10 16:34:53 +0200

    Added edit functionality to To-Do list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,9 +19,20 @@ class TasksController < ApplicationController
   # TO DO:
   #  other basic CRUD actions
 
+  def edit
+    @task = Task.find(params[:id].to_i)
+
+  end
+
+  def update
+    @task = Task.find(params[:id].to_i)
+    @task.update(task_params)
+    redirect_to task_path
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Route for showing *all* tasks
-  get 'tasks',    to: 'tasks#index'
-
-  # Route for showing single tasks
-  get 'task/:id', to: 'tasks#show'
+  get 'tasks', to: 'tasks#index'
 
   # routes for adding a new task
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
 
-  # route for showing one task
-  # get ...
+  # Route for showing single tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # route for editing a task
-  # get ...
-  # patch...
+  # routes for editing a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 
   # route for deleting a task
   # delete...
